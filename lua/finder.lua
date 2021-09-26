db311f7e356841c8eeb5b7011e147b3115844967
gbprod <contact@gb-prod.fr>
2021-09-26 13:52:17 +0200

    format

--- lua/finder.lua
+++ lua/finder.lua
@@ -15,19 +15,45 @@ return {
     local wk = require("which-key")
 
     wk.register({
-      ["<leader>ff"] = { "<cmd>lua require('telescope.builtin').find_files()<cr>", "Find files" },
-      ["<leader>fw"] = { "':Telescope find_files<cr>' . expand('<cword>')", "Find file <cword>", expr = true },
-      ["<leader>fm"] = { "<cmd>Telescope oldfiles cwd_only=true<cr>", "MRU files" },
+      ["<leader>ff"] = {
+        "<cmd>lua require('telescope.builtin').find_files()<cr>",
+        "Find files",
+      },
+      ["<leader>fw"] = {
+        "':Telescope find_files<cr>' . expand('<cword>')",
+        "Find file <cword>",
+        expr = true,
+      },
+      ["<leader>fm"] = {
+        "<cmd>Telescope oldfiles cwd_only=true<cr>",
+        "MRU files",
+      },
       ["<leader>s"] = {
         name = "+Search",
-        s = { "<cmd>lua require('telescope.builtin').live_grep()<cr>", "Live grep" },
-        w = { "<cmd>lua require('telescope.builtin').grep_string()<cr>", "Grep word" },
+        s = {
+          "<cmd>lua require('telescope.builtin').live_grep()<cr>",
+          "Live grep",
+        },
+        w = {
+          "<cmd>lua require('telescope.builtin').grep_string()<cr>",
+          "Grep word",
+        },
       },
-      ["<leader>bb"] = { "<cmd>lua require('telescope.builtin').buffers()<cr>", "Buffers" },
+      ["<leader>bb"] = {
+        "<cmd>lua require('telescope.builtin').buffers()<cr>",
+        "Buffers",
+      },
     }, {
       mode = "n",
     })
 
-    map("n", "<c-p>", "<cmd>lua require('telescope.builtin').find_files()<cr>", {})
+    map(
+      "n",
+      "<c-p>",
+      "<cmd>lua require('telescope.builtin').find_files()<cr>",
+      {}
+    )
+
+    -- lua print(vim.inspect(require'nvim-tree.lib'.get_node_at_cursor()))
   end,
 }
