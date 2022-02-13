e4b6d875964dfa8f0791bf9d86117f5dfbaa5a1c
gbprod <contact@gb-prod.fr>
2022-02-13 14:39:18 +0100

    fterm

--- lua/terminal.lua
+++ lua/terminal.lua
@@ -1,25 +1,50 @@
-return {
-  plugins = function(use)
-    use("numToStr/FTerm.nvim")
-  end,
+local terminal = {}
 
-  setup = function()
-    require("FTerm").setup({
-      dimensions = {
-        height = 0.95,
-        width = 0.95,
-      },
-    })
-  end,
+local custom = {}
 
-  bindings = function(map)
-    map("n", "<M-z>", "<CMD>lua require('FTerm').toggle()<CR>", { noremap = true, silent = true })
-    map("i", "<M-z>", "<esc><CMD>lua require('FTerm').toggle()<CR>", { noremap = true, silent = true })
-    map("t", "<C-o>", [[<C-\><C-n>]], { noremap = true, silent = true })
-    map("t", "<M-z>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', { noremap = true, silent = true })
+function terminal.plugins(use)
+  use("numToStr/FTerm.nvim")
+end
 
-    -- vim.cmd([[
-    -- autocmd TermEnter term://*toggleterm#* set signcolumn=no
-    -- ]])
-  end,
-}
+function terminal.setup()
+  require("FTerm").setup({
+    ft = "FTerm",
+    dimensions = {
+      height = 0.95,
+      width = 0.95,
+    },
+  })
+end
+
+function terminal.bindings(map)
+  map("n", "<M-z>", "<cmd>lua require('FTerm').toggle()<CR>", { noremap = true, silent = true })
+  map("i", "<M-z>", "<esc><cmd>lua require('FTerm').toggle()<CR>", { noremap = true, silent = true })
+  map("t", "<C-o>", [[<C-\><C-n>]], { noremap = true, silent = true })
+
+  vim.cmd([[
+  autocmd TermEnter term://*FTerm#* tnoremap <M-z> <cmd>lua require('FTerm').toggle()<CR>
+]])
+end
+
+function terminal.create_custom(name, cmd)
+  local fterm = require("FTerm")
+
+  custom[name] = fterm:new({
+    cmd = cmd,
+    ft = "FTerm_" .. name,
+  })
+
+  vim.cmd(
+    string.format(
+      [[ autocmd TermEnter term://FTerm_%s lua vim.api.nvim_buf_set_keymap('t', '<M-z>', '<C-\\><C-n><cmd>lua require("terminal").toggle_custom("%s")<CR>', { noremap = true, silent = true }) ]],
+      name,
+      name
+    )
+  )
+end
+
+function terminal.toggle_custom(name)
+  custom[name]:toggle()
+end
+
+return terminal
